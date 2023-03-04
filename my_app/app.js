04be629debe0862e394d691f692d90d68cd700c3
PROFAY <Oyewaletimmy01@gmail.com>
2023-03-04 12:25:03 +0000

    ree

--- my_app/app.js
+++ my_app/app.js
@@ -1,19 +1,28 @@
 const express = require('express');
+const app = express();
 const passport = require('passport');
 const session = require('express-session');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const TwitterStrategy = require('passport-twitter').Strategy;
-const dotenv = require('dotenv');
+const flash = require('connect-flash');
 
+const dotenv = require('dotenv');
 dotenv.config();
 
-const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: process.env.SESSION_SECRET }));
+app.use(express.static('public'));
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -24,63 +33,84 @@ passport.deserializeUser((user, done) => {
 });
 
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
-}, (accessToken, refreshToken, profile, done) => {
-  return done(null, profile);
-}));
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+  },
+  (accessToken, refreshToken, profile, done) => {
+    return done(null, profile);
+  }
+));
 
-passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: '/auth/facebook/callback',
-  profileFields: ['id', 'displayName', 'email', 'picture.type(large)'],
-}, (accessToken, refreshToken, profile, done) => {
-  return done(null, profile);
-}));
+//passport.use(new FacebookStrategy({
+ //   clientID: process.env.FACEBOOK_CLIENT_ID,
+  //  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  //  callbackURL: '/auth/facebook/callback',
+  //  profileFields: ['id', 'displayName', 'email']
+ // },
+  //(accessToken, refreshToken, profile, done) => {
+ //   return done(null, profile);
+  //}
+//));
 
 passport.use(new TwitterStrategy({
-  consumerKey: process.env.TWITTER_CONSUMER_KEY,
-  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-  callbackURL: '/auth/twitter/callback',
-}, (accessToken, refreshToken, profile, done) => {
-  return done(null, profile);
-}));
-
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    callbackURL: '/auth/twitter/callback',
+    includeEmail: true
+  },
+  (accessToken, refreshToken, profile, done) => {
+    return done(null, profile);
+  }
+));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const user = req.user; // define the user variable
+  res.render('index.ejs', { user: user });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+  const user = req.user; // define the user variable
+  res.render('about.ejs', { user: user });
 });
 
 app.get('/login', (req, res) => {
-  res.render('login');
+  const user = req.user; // define the user variable
+  res.render('login.ejs', { user: user }, { error: req.flash('error') });
 });
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  });
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook', { scope: ['email'] }));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  });
+
+app.get('/auth/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  });
+
+app.get('/logout', (req, res) => {
+  req.logout();
   res.redirect('/');
 });
 
-app.get('/auth/facebook', passport.authenticate('facebook'));
-
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
-  res.redirect('/');
-});
-
-app.get('/auth/twitter', passport.authenticate('twitter'));
-
-app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), (req, res) => {
-  res.redirect('/');
-});
-
-app.listen(3000, () => {
-console.log('Server started on http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
