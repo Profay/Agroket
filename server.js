01a31c238505cc0c139f5b44551c9f2ffe8e6911
PROFAY <Oyewaletimmy01@gmail.com>
2023-03-13 11:19:54 +0000

    Update

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const router = require('./router');
+const router = require('./routes');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9000;
 
 app.use(express.json());
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+})
+.catch((err) => {
+  console.log(err);
+});
 app.use('/', router);
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
