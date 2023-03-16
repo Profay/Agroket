8de5a98a8edae86a0c2c7d41c218c9d55425a0c5
PROFAY <Oyewaletimmy01@gmail.com>
2023-03-16 12:47:03 +0000

    update functions

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,71 +2,95 @@ const User = require('../models/userModel');
 
 
 class UserController {
-    static async getProfile(req, res, next){
-        User.findOne({
-            _id: req.decoded.user._id
-          }, (err, user) => {
-            if (err) return next(err);
-            res.json({
-              success: true,
-              user: user,
-              message: "Successful",
-            });
-          });
-        };
-
-    static async updateProfile(req, res, next) {
-        User.findOne({
-            _id: req.decoded.user._id
-          }, (err, user) => {
-            if (err) return next(err);
-        
-            if (req.body.name) user.name = req.body.name;
-            if (req.body.email) user.email = req.body.email;
-            if (req.body.password) user.password = req.body.password;
-        
-            user.isSeller = req.body.isSeller;
-        
-            user.save();
-            res.json({
-              success: true,
-              message: "Profile successfully edited",
-            });
-          });
-    };
-
-    static async getAddress(req, res, next) {
-        User.findOne({
-            _id: req.decoded.user._id
-          }, (err, user) => {
-            res.json({
-              success: true,
-              address: user.address,
-              message: "Successful",
-            });
-          });
+  static async getProfile(req, res, next) {
+    try {
+      const user = await User.findOne({ _id: req.decoded.user._id });
+      res.json({
+        success: true,
+        user: user,
+        message: "Successful",
+      });
+    } catch (err) {
+      next(err);
     }
+  };  
 
-    static async updateAddress(req, res, next) {
-        User.findOne({
-            _id: req.decoded.user._id
-          }, (err, user) => {
-            if (err) return next(err);
-      
-            if (req.body.addr1) user.address.addr1 = req.body.addr1;
-            if (req.body.addr2) user.address.addr2 = req.body.addr2;
-            if (req.body.city) user.address.city = req.body.city;
-            if (req.body.state) user.address.state = req.body.state;
-            if (req.body.country) user.address.country = req.body.country;
-            if (req.body.postalCode) user.address.postalCode = req.body.postalCode;
-      
-            user.save();
-            res.json({
-              success: true,
-              message: "Address successfully edited",
-            });
-          });
-    };
+  static async updateProfile(req, res, next) {
+    try {
+      const user = await User.findOne({ _id: req.decoded.user._id });
+  
+      if (!user) {
+        return res.status(404).json({ success: false, message: "User not found" });
+      }
+  
+      if (req.body.name) {
+        user.name = req.body.name;
+      }
+  
+      if (req.body.email) {
+        user.email = req.body.email;
+      }
+  
+      if (req.body.password) {
+        user.password = req.body.password;
+      }
+  
+      user.isSeller = req.body.isSeller;
+  
+      await user.save();
+  
+      res.json({
+        success: true,
+        message: "Profile successfully edited",
+      });
+    } catch (err) {
+      return next(err);
+    }
+  };
+  
+
+  static async getAddress(req, res, next) {
+    try {
+      const user = await User.findOne({_id: req.decoded.user._id});
+      res.json({
+        success: true,
+        address: user.address,
+        message: "Successful",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  
+
+  static async updateAddress(req, res, next) {
+    try {
+      const user = await User.findOne({ _id: req.decoded.user._id });
+      if (!user) {
+        return res.json({
+          success: false,
+          message: 'User not found',
+        });
+      }
+  
+      if (req.body.addr1) user.address.addr1 = req.body.addr1;
+      if (req.body.addr2) user.address.addr2 = req.body.addr2;
+      if (req.body.city) user.address.city = req.body.city;
+      if (req.body.state) user.address.state = req.body.state;
+      if (req.body.country) user.address.country = req.body.country;
+      if (req.body.postalCode) user.address.postalCode = req.body.postalCode;
+  
+      await user.save();
+  
+      res.json({
+        success: true,
+        message: 'Address successfully edited',
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+  
 }
 
 
