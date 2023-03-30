c6fbbb34c40742828f2f970dd149bea36ba92dc4
PROFAY <85462029+Profay@users.noreply.github.com>
2023-03-30 19:06:10 +0000

    update

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,28 +1,49 @@
-const aws = require("aws-sdk");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
+const {
+  S3
+} = require("@aws-sdk/client-s3");
+const multer = require('multer');
+const multerS3 = require('multer-s3');
 require('dotenv').config();
 
 const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
-const s3 = new aws.S3({
+
+const s3 = new S3({
   accessKeyId: AWS_ACCESS_KEY,
   secretAccessKey: AWS_SECRET_KEY,
+  region: 'eu-west-3'
 });
+
 const upload = multer({
-    storage: multerS3({
-      s3: s3,
-      bucket: "Agroket_data",
-      metadata: function (req, file, cb) {
-        cb(null, {
-          fieldName: file.fieldname
+  storage: multerS3({
+    s3: s3,
+    bucket: 'agroketbucket',
+    metadata: function (req, file, cb) {
+      cb(null, {
+        fieldName: file.fieldname
+      });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
+    }
+  })
+});
+
+function uploadFile(fieldname) {
+  return function(req, res, next) {
+    upload.single(fieldname)(req, res, function(err) {
+      if (err) {
+        return res.status(400).json({
+          success: false,
+          message: 'Error uploading file to S3 bucket',
+          error: err.message
         });
-      },
-      key: function (req, file, cb) {
-        cb(null, Date.now().toString());
-      },
-    }),
-  });
+      } else {
+        next();
+      }
+    });
+  };
+}
 
 
-  module.exports = upload;
+module.exports = uploadFile;
--- routes/seller.js
+++ routes/seller.js
@@ -3,7 +3,7 @@ const SellerController = require("../controllers/SellerController");
 const checkjwt = require("../middlewares/checkjwt");
 
 
-const upload = require("../middlewares/upload");
+const uploadFile = require("../middlewares/upload");
 
 
 
@@ -11,8 +11,8 @@ const upload = require("../middlewares/upload");
 
 router.post("/products/:id", SellerController.findProductById);
 router.get("/products", checkjwt, SellerController.getProducts);
-router.post("/products", [checkjwt, upload.single("product_picture")], SellerController.addProduct);
-router.post("/editproducts", [checkjwt, upload.single("product_picture")], SellerController.updateProduct);
+router.post("/products", [checkjwt, uploadFile("product_picture")], SellerController.addProduct);
+router.post("/editproducts", [checkjwt, uploadFile("product_picture")], SellerController.updateProduct);
 router.get("/hoaxer/test", SellerController.hoaxerTest);
 
 
